4e37fd2f40dcf5f50e3314f0c4b093ae9ff5fbb9
Serhii Hromov <melifarowow@gmail.com>
2022-05-07 10:35:22 +0300

    returns error

--- go.mod
+++ go.mod
@@ -10,6 +10,6 @@ require (
 
 require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	gorm.io/driver/mysql v1.3.3 // indirect
 )
--- init.go
+++ init.go
@@ -1,8 +1,6 @@
 package amoimport
 
 import (
-	"log"
-
 	"gorm.io/gorm"
 )
 
@@ -10,19 +8,20 @@ type ImportService struct {
 	DB *gorm.DB
 }
 
-func Import(db *gorm.DB, leads_path string, contacts_path string, n int) {
+func Import(db *gorm.DB, leads_path string, contacts_path string, n int) error {
 
 	is := &ImportService{DB: db}
 
 	if err := is.Push_Misc(leads_path, n); err != nil {
-		log.Println(err)
+		return err
 	}
 
 	if err := is.Push_Contacts(contacts_path, n); err != nil {
-		log.Println(err)
+		return err
 	}
 
 	if err := is.Push_Leads(leads_path, n); err != nil {
-		log.Println(err)
+		return err
 	}
+	return nil
 }
