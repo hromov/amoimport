882d0d1cab6c75115fd5f5a6d7e75de9b1763c95
Serhii Hromov <melifarowow@gmail.com>
2022-05-07 10:24:42 +0300

    readme

--- init.go
+++ init.go
@@ -10,19 +10,19 @@ type ImportService struct {
 	DB *gorm.DB
 }
 
-func Import(db *gorm.DB, folder string, n int) {
+func Import(db *gorm.DB, leads_path string, contacts_path string, n int) {
 
 	is := &ImportService{DB: db}
 
-	if err := is.Push_Misc(folder+"leads.csv", n); err != nil {
+	if err := is.Push_Misc(leads_path, n); err != nil {
 		log.Println(err)
 	}
 
-	if err := is.Push_Contacts(folder+"contacts.csv", n); err != nil {
+	if err := is.Push_Contacts(contacts_path, n); err != nil {
 		log.Println(err)
 	}
 
-	if err := is.Push_Leads(folder+"leads.csv", n); err != nil {
+	if err := is.Push_Leads(leads_path, n); err != nil {
 		log.Println(err)
 	}
 }
